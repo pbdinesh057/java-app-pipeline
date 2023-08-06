676a8debaea406d2eb8359b59d0624666f6aa2f4
pbdinesh057 <pbdinesh057@gmail.com>
2023-08-06 17:37:45 +0530

    safety branch

--- aws-k8-deployment/modules/k8-mgmt-server/management-server-infra.tf
+++ aws-k8-deployment/modules/k8-mgmt-server/management-server-infra.tf
@@ -1,3 +1,5 @@
+#Safety branch code
+
 resource "aws_instance" "ec2_instance" {
     ami           = var.ami
     instance_type = var.instance_type
@@ -7,6 +9,9 @@ resource "aws_instance" "ec2_instance" {
     
     # Set the key_name attribute to use the SSH key pair name variable
     key_name = var.ssh_key_name
+    provisioner "local-exec" {
+    command = "echo Instance with ${aws_instance.ec2_instance.public_ip} ${aws_instance.ec2_instance.private_ip} Created!! >> /tmp/ec2details.txt"
+  }
     
     # You can add other configuration settings here as needed
 }
@@ -16,4 +21,30 @@ resource "aws_s3_bucket" "s3-backend" {
     #acl    = "private"  # Change this to set the bucket ACL (e.g., "public-read")
     # You can add other configuration settings here as needed, like tags, logging, etc.
 }
+
+
+
+
+  
+
+
+#   connection {
+#     type        = "ssh"
+#     user        = "ubuntu"  # Update with the appropriate user for your AMI
+#     private_key = file("~/.ssh/id_rsa")  # Update with the path to your private key
+#     host        = self.public_ip
+#   }
+
+#   provisioner "file" {
+#     source      = "setup_kubernetes.sh"
+#     destination = "~/setup_kubernetes.sh"
+#   }
+
+#   # Execute the shell script on the instance after it is copied
+#   provisioner "remote-exec" {
+#     inline = [
+#       "chmod +x ~/setup_kubernetes.sh",
+#       "bash ~/setup_kubernetes.sh"
+#     ]
+#   }
 
