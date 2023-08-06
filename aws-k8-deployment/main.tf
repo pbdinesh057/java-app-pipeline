4c237717ce7960f1e9e58da7e236e01249b062d4
pbdinesh057 <pbdinesh057@gmail.com>
2023-08-06 21:15:17 +0530

    added prov block

--- aws-k8-deployment/main.tf
+++ aws-k8-deployment/main.tf
@@ -1,6 +1,6 @@
 module "k8-mgmt" {
   source = "./modules/k8-mgmt-server"  # Path to the directory containing the ec2_instance.tf module file
-  ami               = "ami-0c01437c0b055f2c7"  # Change this to your desired AMI ID for your region
+  #ami               = "ami-0c01437c0b055f2c7"  # Change this to your desired AMI ID for your region
   instance_type     = "t2.medium"  # Change this to your desired EC2 instance type
   instance_name     = "K8-Management-Host"  # Change this to your desired instance name
   vpc_id            = "vpc-07aec5c7932798412"  # Change this to your existing VPC ID
