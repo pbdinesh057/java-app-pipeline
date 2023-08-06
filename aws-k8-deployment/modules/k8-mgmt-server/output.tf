f8bdd5cfa578983ca05e68344ed63d4f97d13f75
pbdinesh057 <pbdinesh057@gmail.com>
2023-08-06 17:49:35 +0530

    added prov block

--- aws-k8-deployment/modules/k8-mgmt-server/output.tf
+++ aws-k8-deployment/modules/k8-mgmt-server/output.tf
@@ -1,19 +1,19 @@
-output "ec2_instance_id" {
-    value = aws_instance.ec2_instance.id
-}
+# output "ec2_instance_id" {
+#     value = aws_instance.ec2_instance.id
+# }
 
-output "ec2_instance_public_ip" {
-    value = aws_instance.ec2_instance.public_ip
-}
+# output "ec2_instance_public_ip" {
+#     value = aws_instance.ec2_instance.public_ip
+# }
 
-output "ec2_instance_public_dns" {
-    value = aws_instance.ec2_instance.public_dns
-}
+# output "ec2_instance_public_dns" {
+#     value = aws_instance.ec2_instance.public_dns
+# }
 
-output "ec2_instance_private_ip" {
-    value = aws_instance.ec2_instance.private_ip
-}
+# output "ec2_instance_private_ip" {
+#     value = aws_instance.ec2_instance.private_ip
+# }
 
-output "s3" {
-    value = aws_s3_bucket.s3-backend.bucket
-}
+# output "s3" {
+#     value = aws_s3_bucket.s3-backend.bucket
+# }
