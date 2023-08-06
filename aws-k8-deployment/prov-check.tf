3c5a59db716d181b3fdede92d2029bfa1249d44b
pbdinesh057 <pbdinesh057@gmail.com>
2023-08-06 17:31:36 +0530

    added terraform modules

---
+++ aws-k8-deployment/prov-check.tf
@@ -0,0 +1,28 @@
+
+provisioner "local-exec" {
+    command = "echo Instance with ${aws_instance.ec2_instance.public_ip} ${aws_instance.ec2_instance.private_ip} Created!! >> /tmp/ec2details.txt"
+  }
+
+  
+
+
+#   connection {
+#     type        = "ssh"
+#     user        = "ubuntu"  # Update with the appropriate user for your AMI
+#     private_key = file("~/.ssh/id_rsa")  # Update with the path to your private key
+#     host        = self.public_ip
+#   }
+
+#   provisioner "file" {
+#     source      = "setup_kubernetes.sh"
+#     destination = "~/setup_kubernetes.sh"
+#   }
+
+#   # Execute the shell script on the instance after it is copied
+#   provisioner "remote-exec" {
+#     inline = [
+#       "chmod +x ~/setup_kubernetes.sh",
+#       "bash ~/setup_kubernetes.sh"
+#     ]
+#   }
+
