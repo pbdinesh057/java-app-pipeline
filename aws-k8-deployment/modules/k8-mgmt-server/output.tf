b398c5abfd2eb74813912ed64f49f24156c7ca55
pbdinesh057 <pbdinesh057@gmail.com>
2023-08-06 17:07:59 +0530

    added terraform modules

--- aws-k8-deployment/modules/k8-mgmt-server/output.tf
+++ aws-k8-deployment/modules/k8-mgmt-server/output.tf
@@ -1,16 +1,16 @@
-output "ec2_instance" {
+output "ec2_instance_id" {
     value = aws_instance.ec2_instance.id
 }
 
-output "ec2_instance" {
+output "ec2_instance_public_ip" {
     value = aws_instance.ec2_instance.public_ip
 }
 
-output "ec2_instance" {
+output "ec2_instance_public_dns" {
     value = aws_instance.ec2_instance.public_dns
 }
 
-output "ec2_instance" {
+output "ec2_instance_private_ip" {
     value = aws_instance.ec2_instance.private_ip
 }
 
