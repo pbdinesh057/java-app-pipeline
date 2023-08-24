80691030df99b1f1b7b873200ca450f53870298f
pbdinesh057 <pbdinesh057@gmail.com>
2023-08-24 20:17:20 +0530

    added simple ec2 creation code in terraform instaed of modules

--- aws-k8-deployment/modules/k8-mgmt-server/management-server-infra.tf
+++ aws-k8-deployment/modules/k8-mgmt-server/management-server-infra.tf
@@ -1,61 +1,61 @@
-# resource "aws_key_pair" "login_key" {
-#   key_name   = var.key_pair_name
-#   public_key = var.public_key
+# # resource "aws_key_pair" "login_key" {
+# #   key_name   = var.key_pair_name
+# #   public_key = var.public_key
+# # }
+
+
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
+
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+#   }
+
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+
+#   owners = ["099720109477"] # Canonical AWS Marketplace account ID for Ubuntu
 # }
 
+# resource "aws_instance" "ec2_instance" {
+#     ami           = "${data.aws_ami.ubuntu.id}"
+#     instance_type = var.instance_type
+#     tags = {
+#         Name = var.instance_name
+#     }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+#     key_name = "all"
+#     #key_name = aws_key_pair.login_key.key_name
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical AWS Marketplace account ID for Ubuntu
-}
-
-resource "aws_instance" "ec2_instance" {
-    ami           = "${data.aws_ami.ubuntu.id}"
-    instance_type = var.instance_type
-    tags = {
-        Name = var.instance_name
-    }
-
-    key_name = "all"
-    #key_name = aws_key_pair.login_key.key_name
-
-    provisioner "local-exec" {
-    command = "echo Instance with ${aws_instance.ec2_instance.public_ip} ${aws_instance.ec2_instance.private_ip} Created!! >> /tmp/ec2details.txt"
-  }
-#     connection {
-#     type        = "ssh"
-#     user        = "ubuntu"  # Update with the appropriate user for your AMI
-#     private_key = file("/tmp/all.pem")  # Update with the path to your private key
-#     host        = self.public_ip
+#     provisioner "local-exec" {
+#     command = "echo Instance with ${aws_instance.ec2_instance.public_ip} ${aws_instance.ec2_instance.private_ip} Created!! >> /tmp/ec2details.txt"
 #   }
-#     provisioner "file" {
-#     source      = "setup_kubernetes.sh"
-#     destination = "/tmp/setup_kubernetes.sh"
-#   }
-    # Execute the shell script on the instance after it is copied
-#   provisioner "remote-exec" {
-#     inline = [
-#       "chmod +x /tmp/setup_kubernetes.sh",
-#       "bash /tmp/setup_kubernetes.sh"
-#     ]
-#   }
-    # Set the key_name attribute to use the SSH key pair name variable
-}
+# #     connection {
+# #     type        = "ssh"
+# #     user        = "ubuntu"  # Update with the appropriate user for your AMI
+# #     private_key = file("/tmp/all.pem")  # Update with the path to your private key
+# #     host        = self.public_ip
+# #   }
+# #     provisioner "file" {
+# #     source      = "setup_kubernetes.sh"
+# #     destination = "/tmp/setup_kubernetes.sh"
+# #   }
+#     # Execute the shell script on the instance after it is copied
+# #   provisioner "remote-exec" {
+# #     inline = [
+# #       "chmod +x /tmp/setup_kubernetes.sh",
+# #       "bash /tmp/setup_kubernetes.sh"
+# #     ]
+# #   }
+#     # Set the key_name attribute to use the SSH key pair name variable
+# }
 
-resource "aws_s3_bucket" "s3-backend" {
-    bucket = var.bucket_name
-    #acl    = "private"  # Change this to set the bucket ACL (e.g., "public-read")
-    # You can add other configuration settings here as needed, like tags, logging, etc.
-}
+# resource "aws_s3_bucket" "s3-backend" {
+#     bucket = var.bucket_name
+#     #acl    = "private"  # Change this to set the bucket ACL (e.g., "public-read")
+#     # You can add other configuration settings here as needed, like tags, logging, etc.
+# }
 
--- aws-k8-deployment/modules/k8-mgmt-server/variables.tf
+++ aws-k8-deployment/modules/k8-mgmt-server/variables.tf
@@ -1,60 +1,60 @@
-# variable "ami" {
+# # variable "ami" {
+# #     type    = string
+# #     default = "ami-0c01437c0b055f2c7"
+# # }
+
+# variable "instance_type" {
 #     type    = string
-#     default = "ami-0c01437c0b055f2c7"
+#     default = "t2.medium"
 # }
 
-variable "instance_type" {
-    type    = string
-    default = "t2.medium"
-}
+# variable "instance_name" {
+#     type    = string
+#     default = "K8-Management-Host"
+# }
 
-variable "instance_name" {
-    type    = string
-    default = "K8-Management-Host"
-}
+# variable "security_group_id" {
+#     type    = string
+#     default = "sg-057e80a9d74ea91b8"
+# }
 
-variable "security_group_id" {
-    type    = string
-    default = "sg-057e80a9d74ea91b8"
-}
+# variable "subnet_id" {
+#     type    = string
+#     default = "subnet-0033d0e03ff2e6035"
+# }
 
-variable "subnet_id" {
-    type    = string
-    default = "subnet-0033d0e03ff2e6035"
-}
+# variable "vpc_id" {
+#     type    = string
+#     default = "vpc-07aec5c7932798412"
+# }
 
-variable "vpc_id" {
-    type    = string
-    default = "vpc-07aec5c7932798412"
-}
+# variable "bucket_name" {
+#     type    = string
+#     default = "k8-config-backend"
+# }
 
-variable "bucket_name" {
-    type    = string
-    default = "k8-config-backend"
-}
+# variable "aws_region" {
+#     type    = string
+#     default = "us-east-1"
+# }
 
-variable "aws_region" {
-    type    = string
-    default = "us-east-1"
-}
-
-variable "tags" {
-    type = string
-}
-
-# Add a new variable for the SSH key pair name
-# variable "ssh_key_name" {
+# variable "tags" {
 #     type = string
-#     # Provide a default value or make it required, depending on your use case
-#     default = "/tmp/all.pem"
-# }
-# variable "key_pair_name" {
-#   type    = string
-#   default = "login_key"
 # }
 
+# # Add a new variable for the SSH key pair name
+# # variable "ssh_key_name" {
+# #     type = string
+# #     # Provide a default value or make it required, depending on your use case
+# #     default = "/tmp/all.pem"
+# # }
+# # variable "key_pair_name" {
+# #   type    = string
+# #   default = "login_key"
+# # }
 
-# variable "public_key" {
-#   type    = string
-#   default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDiWe4LhkSJUF/A0hxIrbq5gzZRP/RRniwYxpDdINMXbvrClyUAQgJpmzsok8v2HVZKpSfmlgKv2ODAHPU/d6trW6zevZO0oQMGhUaPKusY4umzyDvkr2nSE8rV8El8G/3AmkHSItr+yJUMf+3PI3teA/bsw2YrVb28pI0OB0PtK3mBwWgz9lz/ZYIV2q1GyxPvW7uZc7yr+co/db18O4yXaTkaJLS7EdpyiovGyuvTIN0LjqZlQO8nXU4cKVTwVSvpohlYE5gjU+g9W3ryCNIH3eb6QzepePZfg+qlTPBYlw7M52YqD55YTsBBfut3tlPDsDFLGJknBG6P9QGVQpu1 imported-openssh-key"
-# }
+
+# # variable "public_key" {
+# #   type    = string
+# #   default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDiWe4LhkSJUF/A0hxIrbq5gzZRP/RRniwYxpDdINMXbvrClyUAQgJpmzsok8v2HVZKpSfmlgKv2ODAHPU/d6trW6zevZO0oQMGhUaPKusY4umzyDvkr2nSE8rV8El8G/3AmkHSItr+yJUMf+3PI3teA/bsw2YrVb28pI0OB0PtK3mBwWgz9lz/ZYIV2q1GyxPvW7uZc7yr+co/db18O4yXaTkaJLS7EdpyiovGyuvTIN0LjqZlQO8nXU4cKVTwVSvpohlYE5gjU+g9W3ryCNIH3eb6QzepePZfg+qlTPBYlw7M52YqD55YTsBBfut3tlPDsDFLGJknBG6P9QGVQpu1 imported-openssh-key"
+# # }
